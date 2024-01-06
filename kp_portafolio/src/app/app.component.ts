9ebde66bea495337a672327893415dbfab577656
DESKTOP-EIGD948\PC <kevinpaco40@gmail.com>
2024-01-06 18:04:23 -0300

    se sube actualizacion en los proyectos

--- kp_portafolio/src/app/app.component.ts
+++ kp_portafolio/src/app/app.component.ts
@@ -15,4 +15,15 @@ export class AppComponent {
     const href = './assets/archivos/Kevin Paco-CV-2023-12-03.pdf';
     saveAs.saveAs(href,"Kevin Paco-CV.pdf");
   }
+
+  descargarManualPlayaApp(){
+    const href = './assets/archivos/Manual de Playa App.pdf';
+    saveAs.saveAs(href,"Manual-Playa App.pdf");
+  }
+   
+  
+  descargarManualOptica(){
+    const href = './assets/archivos/Manual de Usuario - OpticaEight.pdf';
+    saveAs.saveAs(href,"Manual-Optica Eight.pdf");
+  }
 }
